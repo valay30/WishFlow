8f2dfbd436ab80821700f7fb078d07c97d4a3973
valay30 <patelvalay308@gmail.com>
2026-08-18 15:22:06 +0530

    bottom space added

--- src/pages/SharedCollection.jsx
+++ src/pages/SharedCollection.jsx
@@ -605,7 +605,7 @@ export default function SharedCollection() {
       min-height: 100vh;
       background: #fafafa;
       font-family: 'Outfit', sans-serif;
-      padding-bottom: 50px;
+      padding-bottom: 80px;
     }
     @media (min-width: 640px) {
       .sc-page {
